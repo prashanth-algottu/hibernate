bc71a3008535c099a90f1f822a68542d508c4216
DELL <DELL@Chinnu>
2022-03-20 22:06:46 +0530

    SCSDC

--- HibernateSecond/src/main/java/com/tectoro/HibernateSecond/App.java
+++ HibernateSecond/src/main/java/com/tectoro/HibernateSecond/App.java
@@ -23,17 +23,39 @@ public class App
         SessionFactory factory = cfg.buildSessionFactory();
        // System.out.println(factory);
         
-        Details d = new Details(1, "chinnu");
+     /*   Details d = new Details(6, "latha");
         System.out.println(d);
         Session session = factory.openSession();
-        Transaction transaction = session.beginTransaction();
-        FileInputStream inputStream = new FileInputStream("src/main/java/sree.jpg");
-        byte[] data = new byte[inputStream.available()];
+        Transaction transaction = session.beginTransaction(); */
         
-        inputStream.read();
-        d.setImg(data);
-        session.save(d);
-        transaction.commit();
+        // If you want to save a image this is the process
+       // FileInputStream inputStream = new FileInputStream("src/main/java/sree.jpg");
+        //byte[] data = new byte[inputStream.available()];
+        //inputStream.read();
+        //d.setImg(data);
+        
+        
+    /*    session.save(d);
+        transaction.commit(); */
+        
+        // If you want to fetch values 
+        // take session as previous , we can do it by using session
+        Session session = factory.openSession();
+        // get()
+        Details details = session.get(Details.class, 1); 
+        System.out.println(details);
+        
+        Details details1 = session.get(Details.class, 1000); // gives null
+        System.out.println(details1);
+        
+        // load()
+        
+        Details load = session.load(Details.class, 2);  
+        System.out.println(load);
+        
+        Details load1 = session.load(Details.class, 1002); // gives ObjectNotFoundException 
+        System.out.println(load1);
+        
         
     }
 }
